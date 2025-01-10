7d8543bfc51ce3e29a49fcc1f11ee7764819a25f
Suraz Agrahari <iamsuraz67@gmail.com>
2025-01-10 23:19:59 +0545

    feat:part1

---
+++ part1/changes.js
@@ -0,0 +1,9 @@
+let gameName="spiderman";
+
+gameName="batman"
+
+console.log(gameName)//batman
+
+const game="ff";
+game="pubg";
+console.log("game")//constant we cannot change
---
+++ part1/printing.js
@@ -0,0 +1,5 @@
+console.log("Hello");//!simply print in console
+console.log("suraz");
+
+console.table({city:"Butwal "});//!table kind of console
+console.warn("JavaScript")//!it is kind of warning
