ca0e6c22526811d6bfaec789279d3783c920163a
Suraz Agrahari <iamsuraz67@gmail.com>
2025-01-11 13:08:19 +0545

    basics again

--- part1/changes.js
+++ part1/changes.js
@@ -1,9 +1,10 @@
 let gameName="spiderman";
 
-gameName="batman"
+gameName="batman"//!changes to batman
 
 console.log(gameName)//batman
 
 const game="ff";
 game="pubg";
 console.log("game")//constant we cannot change
+
---
+++ part1/logical.js
@@ -0,0 +1,9 @@
+//!      &&-> and
+//!      || -> or
+//!      ! -> reverse (not)
+
+let isLoggedIn = true;
+let isPaid = false;
+
+console.log(isLoggedIn && isPaid);//!True  if both are true.
+console.log(isLoggedIn || isPaid);//!True if anyone of both is true
